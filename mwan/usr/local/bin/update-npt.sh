107a0739c0a6fed7bda06cef138865856192c632
Alexander Goodkind <alex@goodkind.io>
2025-12-08 13:24:05 -0800

    Remove reboot tasks from deploy-mwan.yml and update network configuration in 20-att.network.j2 to include static IP and routing for SFP module. Add new update-npt.sh script for managing IPv6 prefix translation rules.
    Signed-off-by: Alex Goodkind <alex@goodkind.io>

---
+++ mwan/usr/local/bin/update-npt.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Update nftables NPT (IPv6 prefix translation) rules
+# Called by dhcpcd.exit-hook when prefix delegation changes
+# NOTE: This file is templated by Ansible - see update-npt.sh.j2
+
+set -e
+
+WAN_IFACE="$1"
+DELEGATED_PREFIX="$2"
+
+if [ -z "$WAN_IFACE" ] || [ -z "$DELEGATED_PREFIX" ]; then
+    echo "Usage: $(basename "$0") <wan_iface> <delegated_prefix>"
+    echo "Example: $(basename "$0") enatt0.3242 2600:1700:2f71:c80::/60"
+    exit 1
+fi
+
+readonly INTERNAL_PREFIX="3d06:bad:b01::/56"
+
+log() {
+    logger -t update-npt "$1"
+    echo "[update-npt] $1"
+}
+
+log "Updating NPT rules for $WAN_IFACE"
+log "  Delegated prefix: $DELEGATED_PREFIX"
+
+# Delete existing postrouting rules
+for handle in $(nft -a list chain ip6 nat postrouting | grep "oif \"$WAN_IFACE\".*# handle" | sed -e 's/^.* handle //'); do
+    nft delete rule ip6 nat postrouting handle "$handle"
+done
+
+# Delete existing prerouting rules
+for handle in $(nft -a list chain ip6 nat prerouting | grep "iif \"$WAN_IFACE\".*# handle" | sed -e 's/^.* handle //'); do
+    nft delete rule ip6 nat prerouting handle "$handle"
+done
+
+# Postrouting (outbound): NPT internal prefix to ISP prefix
+nft add rule ip6 nat postrouting oif "$WAN_IFACE" ip6 saddr "$INTERNAL_PREFIX" snat ip6 prefix to "$DELEGATED_PREFIX"
+
+# Prerouting (inbound): NPT ISP prefix back to internal prefix
+nft add rule ip6 nat prerouting iif "$WAN_IFACE" ip6 daddr "$DELEGATED_PREFIX" dnat ip6 prefix to "$INTERNAL_PREFIX"
+
+# Save current nftables config
+nft list ruleset > /etc/nftables.conf.dynamic
+
+log "NPT update complete for $WAN_IFACE"
